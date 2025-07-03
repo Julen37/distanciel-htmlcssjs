629efee087115880ee5ad7c1ff58bf53e1d1e08c
Julen37 <julen.gouchault@gmail.com>
2025-07-03 09:02:25 +0200

    update commentaire

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // popup
 window.onload = function(){ // affiche la popup automatiquement au chargement de la page
-    document.getElementById("popup").classList.add("active"); 
+    document.getElementById("popup").classList.add("active"); //active est un class créé dans le css
 };
 
-document.getElementById("closeBtn").onclick = function(){
-    document.getElementById("popup").classList.remove("active"); // active ? natif a js ?
+document.getElementById("closeBtn").onclick = function(){ // ferme la popup quand on appui sur la croix du span
+    document.getElementById("popup").classList.remove("active"); 
 };
